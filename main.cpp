6a27040be7a5a8b2905c11282d8cf4e9e054576b
Sanjay Yepuri <sanjay.yepuri@gmail.com>
2021-03-27 20:41:04 -0500

    fixed bug

--- main.cpp
+++ main.cpp
@@ -17,59 +17,60 @@ namespace kmeans {
 void parse_cmdargs(kmeans::args_t &opts, int argc, char **argv)
 {
     int c;
-    switch ((c = getopt(argc, argv, "k:d:i:m:t:cs:gfp")) != -1))
+    while ((c = getopt(argc, argv, "k:d:i:m:t:cs:gfp")) != -1)
     {
-        case 'k':
-            opts.k = stoi(optarg);
-            break;
+        switch (c) {
+            case 'k':
+                opts.k = std::stoi(optarg);
+                break;
 
-        case 'd':
-            opts.dims = stoi(optarg);
-            break;
+            case 'd':
+                opts.dims = std::stoi(optarg);
+                break;
 
-        case 'i':
-            opts.input_file = optarg;
-            break;
+            case 'i':
+                opts.input_file = optarg;
+                break;
 
-        case 'm':
-            opts.max_iteers = optarg;
-            break;
+            case 'm':
+                opts.max_iters = std::stoi(optarg);
+                break;
 
-        case 't':
-            opts.threshold = stof(optarg);
-            break;
+            case 't':
+                opts.threshold = std::stof(optarg);
+                break;
 
-        case 'c':
-            opts.print_centroids = true;
-            break;
+            case 'c':
+                opts.print_centroids = true;
+                break;
 
-        case 's':
-            opts.rand_seed = stoi(optarg);
-            break;
+            case 's':
+                opts.rand_seed = std::stoi(optarg);
+                break;
 
-        case 'g':
-            opts.gpu = true;
-            break;
+            case 'g':
+                opts.gpu = true;
+                break;
 
-        case 'f':
-            opts.gpu_shmem = true;
-            break;
+            case 'f':
+                opts.gpu_shmem = true;
+                break;
 
-        case 'p':
-            opts.kmeans_pp = true;
-            break;
+            case 'p':
+                opts.kmeans_pp = true;
+                break;
 
-        default:
-            abort();
-            break;
+            default:
+                abort();
+                break;
+        }
     }
-
 }
 
 int main(int argc, char **argv)
 {
-    kmeans:opts_t options;
-    parse_cmdargs(&options, argc, argv);a
+    kmeans::args_t options;
+    parse_cmdargs(options, argc, argv);
 
-
+    return 0;
 }
